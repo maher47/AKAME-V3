36710832ffe88b1e2051c9850a075ce11b502797
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-12-20 10:02:24 +0100

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -1,51 +1,25 @@
 module.exports.config = {
-	name: "join",
-	eventType: ["log:subscribe"],
-	version: "1.0.1",
-	credits: "ryuko",
-	description: "join and welcome notification",
-	dependencies: {
-		"fs-extra": ""
-	}
+    name: "joinNoti",
+    eventType: ["log:subscribe"],
+    version: "1.0.4",
+  credits: "S H A D O W",
+    description: "notifi member join",
+    dependencies: {
+        "fs-extra": ""
+    }
 };
 
-module.exports.run = async function({ api, event,Threads }) {
-	const { join } = global.nodemodule["path"];
-	const { threadID } = event;
-	const data = (await Threads.getData(event.threadID)).data || {};
-    const checkban = data.banOut || []
-	if  (checkban.includes(checkban[0])) return
-	else if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
-api.changeNickname(`${(!global.config.BOTNAME) ? "ryuko" : global.config.BOTNAME} ai`, threadID, api.getCurrentUserID());
-		return api.sendMessage(`bot connected successfully\n\nabout me?\nbot name : ${global.config.BOTNAME}\nbot prefix : ${global.config.PREFIX}\n\nbot data?\nusers : ${global.data.allUserID.length}\ngroups : ${global.data.allThreadID.length}\n\nhow to use?\n${global.config.PREFIX}help (command list)\n${global.config.BOTNAME} (question) - no prefix\ntalk (text) - no prefix\n\nthankyou for using ${global.config.BOTNAME} ai.`, threadID);
-	}
-	else {
-		try {
-			const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-			let { threadName, participantIDs } = await api.getThreadInfo(threadID);
-
-			const threadData = global.data.threadData.get(parseInt(threadID)) || {};
-
-			var mentions = [], nameArray = [], memLength = [], i = 0;
-			
-			for (id in event.logMessageData.addedParticipants) {
-				const userName = event.logMessageData.addedParticipants[id].fullName;
-				nameArray.push(userName);
-				mentions.push({ tag: userName, id });
-				memLength.push(participantIDs.length - i++);
-			}
-			memLength.sort((a, b) => a - b);
-			
-			(typeof threadData.customJoin == "undefined") ? msg = "hello, {name}. welcome to {threadName}." : msg = threadData.customJoin;
-			msg = msg
-			.replace(/\{name}/g, nameArray.join(', '))
-			.replace(/\{type}/g, (memLength.length > 1) ?  'friends' : 'you')
-			.replace(/\{soThanhVien}/g, memLength.join(', '))
-			.replace(/\{threadName}/g, threadName);
-
-			let formPush = { body: msg, mentions }
-
-			return api.sendMessage(formPush, threadID);
-		} catch (e) { return console.log(e) };
-	}
-}
+module.exports.run = async function({ api, event, Users, Threads }) {
+   var fullYear = global.client.getTime("fullYear");
+  	var getHours = await global.client.getTime("hours");
+			var session = `${getHours < 3 ? "بعد منتصف الليل" : getHours < 8 ? "الصباح الباكر" : getHours < 11 ? "وقت الظهيرة" : getHours < 16 ? "قبل الظهر" : getHours < 23 ? "الليل" : "منتصف الليل"}`
+    const { join } = global.nodemodule["path"];
+    const { threadID } = event;
+  const { PREFIX } = global.config;
+    console.log(2)
+    if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
+        console.log(1)
+        return api.sendMessage("⌯ تم التفعيل بنجاح", threadID, async () => {
+            let check = true;
+            while (check) {
+                setTimeout(() => check = false, 30 * 1000);
